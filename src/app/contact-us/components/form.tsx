c4dcedccd7df9e3fa58b086914291de413f0e914
mahsabadiei <mahsa@babco.co>
2025-01-29 22:35:30 +0200

    Fix form error message ui

--- src/app/contact-us/components/form.tsx
+++ src/app/contact-us/components/form.tsx
@@ -81,9 +81,12 @@ const ContactForm = () => {
     <div className="w-full flex flex-col items-start justify-center">
       {apiRes.message && (
         <div
-          className={`w-full flex flex-row gap-2 items-center justify-start p-2 mb-10 rounded bg-white/10 ${
-            apiRes.status === "fail" ? "text-error" : ""
-          }`}
+          className={`w-full flex flex-row gap-2 items-center justify-start p-2 mb-10 rounded 
+            ${
+              apiRes.status === "success"
+                ? "bg-black/10 dark:bg-white/10 text-black dark:text-white"
+                : "bg-black/10 text-error"
+            }`}
         >
           {apiRes.status === "success" ? (
             <svg
@@ -95,7 +98,7 @@ const ContactForm = () => {
             >
               <path
                 d="M10 0C4.49 0 0 4.49 0 10C0 15.51 4.49 20 10 20C15.51 20 20 15.51 20 10C20 4.49 15.51 0 10 0ZM14.78 7.7L9.11 13.37C8.97 13.51 8.78 13.59 8.58 13.59C8.38 13.59 8.19 13.51 8.05 13.37L5.22 10.54C4.93 10.25 4.93 9.77 5.22 9.48C5.51 9.19 5.99 9.19 6.28 9.48L8.58 11.78L13.72 6.64C14.01 6.35 14.49 6.35 14.78 6.64C15.07 6.93 15.07 7.4 14.78 7.7Z"
-                fill="white"
+                className="fill-black dark:fill-white"
               />
             </svg>
           ) : (
